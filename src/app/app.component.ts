dea23660abcdc73de3f1cadde282871f5f283a66
Antonio Jesus Ríos Cabrera <antonio.rios.cabrera@avanade.com>
2018-06-20 16:10:05 +0200

    Welcome Page in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Euroscoreboard Official Website';
+  _currentYear: number;
+  get currentYear(): number {
+    this._currentYear = (new Date(Date.now())).getFullYear();
+    return this._currentYear;
+  }
+
 }
