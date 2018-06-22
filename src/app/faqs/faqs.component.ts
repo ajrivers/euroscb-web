21083e062d68ffc060499489d64a23d159e9e132
Antonio Jesus Ríos Cabrera <antonio.rios.cabrera@avanade.com>
2018-06-22 14:37:39 +0200

    Downloads Section almost complete (Javascript to be fixed) 2

--- src/app/faqs/faqs.component.ts
+++ src/app/faqs/faqs.component.ts
@@ -1,10 +1,9 @@
-import { IQuestion, QuestionType, IFaqTypeDescription } from "./question";
+import { IQuestion, IFaqTypeDescription } from "./question";
 import { Component, OnInit } from '@angular/core';
 import { FaqsService } from './faqs.service';
-import { QuestionFilterPipe } from './question.pipe'
 
 @Component({
-  selector: 'app-faqs',  
+  selector: 'app-faqs',
   templateUrl: './faqs.component.html',
   styleUrls: ['./faqs.component.css']
 })
@@ -12,19 +11,6 @@ export class FaqsComponent implements OnInit {
 
   questionList: IQuestion[];
   questionTypes: IFaqTypeDescription[];
-
-  // _questionTypes: string[];
-  // get questionTypes(): string[] {
-  //   return Object.keys(QuestionType);
-  // }
-
-  // _faqTypes: IFaqTypeDescription[];
-  // get faqTypes(): IFaqTypeDescription[] {
-  //   this._faqsService.getServerData().subscribe(
-  //     faqTypes => this._faqTypes = faqTypes.faqTypesArray
-  //   );
-  //   return this._faqTypes;
-  // }
 
   constructor(private _faqsService: FaqsService) { }
 
---
+++ src/assets/js/site.js
@@ -0,0 +1,16 @@
+(function(){
+    "use strict";
+
+    var $downloadLinks = $("#download-link-*");
+    var $notAvailable = $("#notAvailable");
+
+    $downloadLinks.each(function() {
+        if(this.href === '#')
+        {
+            this.on("click", function() {
+                $notAvailable.modal('show');
+                return false;
+            });
+        }
+    });
+})();
