daf8af8b972ab4ea99b8a0c0d85362d0c45b1f28
Antonio Jesus Ríos Cabrera <antonio.rios.cabrera@avanade.com>
2018-06-29 14:07:04 +0200

    Design Filters done!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
@@ -27,7 +28,8 @@ import { DesignListComponent } from './designs/design-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    HttpClientModule,    
+    FormsModule,
     RouterModule.forRoot([
       { path:'welcome', component: WelcomeComponent },
       { path: 'faqs', component: FaqsComponent },
--- src/app/designs/design-list.component.ts
+++ src/app/designs/design-list.component.ts
@@ -11,10 +11,35 @@ import { Observable } from 'rxjs';
 })
 export class DesignListComponent implements OnInit {
 
-  //#region Properties
+  //#region Simple Properties
 
   fileUrl: string = '../assets/api/designs.json';
   designItems: IDesignItem[];
+  filteredDesignItems: IDesignItem[];
+  errorMessage: string;
+  designVersions: string[];
+
+  //#endregion
+
+  //#region Defined Properties
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredDesignItems = this.designItems ? this.performFilter(this._listFilter, this._typeFilter) : this.designItems;
+  }
+
+  _typeFilter: string;
+  get typeFilter(): string {
+    return this._typeFilter;
+  }
+  set typeFilter(value: string) {
+    this._typeFilter = value;
+    this.filteredDesignItems = this.designItems ? this.performFilter(this._listFilter, this._typeFilter) : this.designItems;
+  }
 
   //#endregion
 
@@ -26,20 +51,35 @@ export class DesignListComponent implements OnInit {
 
   //#region Public Methods
 
-  public getDesignItems(): Observable<IDesignItem[]>{
+  public getDesignItems(): Observable<IDesignItem[]> {
     return this._http.get<IDesignItem[]>(this.fileUrl)
-    .pipe(
-      catchError(this.handleError)
-    );
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   //#endregion
 
   //#region Private Methods
 
-  private handleError(err){
+  private handleError(err) {
     console.log(err.message);
     return Observable.throw(err.message);
+  }
+
+  private performFilter(filterBy: string, typeFilter: string): IDesignItem[] {
+    filterBy = !(filterBy == null || filterBy == undefined) ? filterBy.toLocaleLowerCase() : filterBy;
+    typeFilter = !(typeFilter == null || typeFilter == undefined) ? typeFilter.toLocaleLowerCase() : typeFilter;
+    let auxiliarArray: IDesignItem[] = this.designItems;
+
+    if (filterBy !== "" && filterBy != null && filterBy != undefined) {
+      auxiliarArray = auxiliarArray.filter((item: IDesignItem) => item.name.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+    if (typeFilter !== "" && typeFilter != null && typeFilter != undefined) {
+      auxiliarArray = auxiliarArray.filter((item: IDesignItem) => item.euroscbVersion.toLocaleLowerCase().indexOf(typeFilter) !== -1);
+    }
+
+    return auxiliarArray;
   }
 
   //#endregion
@@ -48,7 +88,13 @@ export class DesignListComponent implements OnInit {
 
   ngOnInit() {
     this.getDesignItems().subscribe(
-      items => this.designItems = items
+      items => {
+        this.designItems = items;
+        this.filteredDesignItems = this.designItems;
+        this.designVersions = Array.from(new Set(this.designItems.map(item => item.euroscbVersion)));
+        this.designVersions.splice(0, 0, '');
+      },
+      error => this.errorMessage = <any>error
     );
   }
 
